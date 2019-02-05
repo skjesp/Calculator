ea5b356fa68c9fdfa02129baca1de232d31f2fc0
Johannes Sebastian K C Jespersen <201606235@post.au.dk>
2019-02-05 13:40:28 +0100

    Add project files.

---
+++ Calculator.Unit.Test/Calculator.Unit.Test.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.Unit.Test
+{
+    [TestFixture]
+    public class CalculatorUnitTests
+    {
+        [Test]
+        public void Add_Add4and2_return6() //Bemærk navngivningen
+        {
+            var uut = new Calculator();
+            Assert.That(6,Is.EqualTo(uut.Add(4,2)));
+        }
+
+        [Test]
+        public void Subtract_Sub8and3_return5()
+        {
+            var uut = new Calculator();
+            Assert.That(5,Is.EqualTo(uut.Subtract(8,3)));
+        }
+
+        [Test]
+        public void Multiply_Mul5and5_return25()
+        {
+            var uut = new Calculator();
+            Assert.That(25,Is.EqualTo(uut.Multiply(5,5)));
+        }
+
+        [Test]
+        public void Power_pow3and4_return81()
+        {
+            var uut = new Calculator();
+            Assert.That(81,Is.EqualTo(uut.Power(3,4)));
+        }
+    }
+}
---
+++ Calculator/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Calculator
+{
+    public class Calculator
+    {
+        static void Main()
+        {
+            return;
+        }
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            return System.Math.Pow(x, exp);
+        }
+    }
+}
